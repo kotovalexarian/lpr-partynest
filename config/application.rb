815ca02aa4ec2a4f85de071179c0222b10709ff9
Alex Kotov <kotovalexarian@gmail.com>
2018-11-23 01:04:28 +0500

    Fix code style

--- config/application.rb
+++ config/application.rb
@@ -24,8 +24,8 @@ module Partynest
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
+    # Settings in config/environments/* take precedence over those specified
+    # here. Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
