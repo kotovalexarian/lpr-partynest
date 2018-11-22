9cc56f43c95e767772a411be0c0267508d1ab46b
Alex Kotov <kotovalexarian@gmail.com>
2018-11-23 01:15:08 +0500

    Add dummy specs

---
+++ spec/requests/default_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'GET /' do
+  before do
+    get '/'
+  end
+
+  specify do
+    expect(response).to have_http_status :ok
+  end
+
+  specify do
+    expect(response.body).to match 'Hello, World!'
+  end
+end
