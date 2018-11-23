194db94483561e1e23c239320571a63ca4827fba
Alex Kotov <kotovalexarian@gmail.com>
2018-11-23 23:21:52 +0500

    Add action MembershipApplications#new

---
+++ app/controllers/membership_applications_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class MembershipApplicationsController < ApplicationController
+  # GET /membership_applications/new
+  def new; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@
 
 Rails.application.routes.draw do
   root to: ->(_) { [200, {}, ['Hello, World!']] }
+
+  resources :membership_applications, only: :new
 end
