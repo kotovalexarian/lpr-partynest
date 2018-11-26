5aae578d8c2ff124c12e4b8fdc86972e6b0e6e4f
Alex Kotov <kotovalexarian@gmail.com>
2018-11-26 20:19:07 +0500

    Configure locales

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -2,7 +2,7 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.i18n.default_locale = :'ru-RU'
-Rails.application.config.i18n.available_locales = %i[en-US ru-RU]
+Rails.application.config.i18n.default_locale = :ru
+Rails.application.config.i18n.available_locales = %i[en ru]
 Rails.application.config.i18n.load_path +=
   Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
