27ea908ac7d569f675a2abbb06bd36d1fe5978b7
Alex Kotov <kotovalexarian@gmail.com>
2018-11-29 01:42:47 +0500

    Add old content

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'home#show'
 
   get '/events/2018/11/24/otkrytaja-vstrecha-storonnikov-libertarianskoj-partii.html' => 'home#event1'
+  get '/posts/2018/07/27/sformirovan-rukovodjashhij-komitet-ro-lpr-v-permskom-krae.html' => 'home#post1'
   get '/posts/2018/11/12/sekretar-ro-lpr-v-permskom-krae-byl-zaderzhan-na-4-chasa.html' => 'home#post2'
 
   resources :membership_applications, only: %i[new create]
