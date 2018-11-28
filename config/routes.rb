972e1c5bdc6bdba4b06d371d4d9b12c7b944b84a
Alex Kotov <kotovalexarian@gmail.com>
2018-11-29 01:30:11 +0500

    Add old content

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@
 Rails.application.routes.draw do
   root to: 'home#show'
 
+  get '/events/2018/11/24/otkrytaja-vstrecha-storonnikov-libertarianskoj-partii.html' => 'home#event1'
+
   resources :membership_applications, only: %i[new create]
 end
