a8d2ea30318cd55487927cac14d9e8d17a458f3d
Alex Kotov <kotovalexarian@gmail.com>
2018-11-29 01:48:37 +0500

    Add old shortlinks

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@
 Rails.application.routes.draw do
   root to: 'home#show'
 
+  get '/s/452465', to: 'home#event1'
+  get '/s/243898', to: 'home#post1'
+  get '/s/377295', to: 'home#post2'
+
   get '/events/2018/11/24/otkrytaja-vstrecha-storonnikov-libertarianskoj-partii.html' => 'home#event1'
   get '/posts/2018/07/27/sformirovan-rukovodjashhij-komitet-ro-lpr-v-permskom-krae.html' => 'home#post1'
   get '/posts/2018/11/12/sekretar-ro-lpr-v-permskom-krae-byl-zaderzhan-na-4-chasa.html' => 'home#post2'
