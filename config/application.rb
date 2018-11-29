37ee630cdc2bba3de9ebd28cc05e6f7cbc9aa630
Alex Kotov <kotovalexarian@gmail.com>
2018-11-30 03:44:18 +0500

    Fix code style

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Partynest
       password
       password_confirmation
       secret
-    ]
+    ].freeze
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
