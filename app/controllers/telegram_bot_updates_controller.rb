ce8269c4139fd050ffb6db81ec2ceaf98a3c275d
Alex Kotov <kotovalexarian@gmail.com>
2018-11-29 20:42:30 +0500

    Remove unnecessary code

--- app/controllers/telegram_bot_updates_controller.rb
+++ app/controllers/telegram_bot_updates_controller.rb
@@ -8,8 +8,6 @@ class TelegramBotUpdatesController < ApplicationController
   def create
     logger.info params.inspect
 
-    handle_message params[:message] if params[:message]
-
     render status: :no_content, json: {}
   end
 
@@ -21,16 +19,5 @@ private
 
   def verify_telegram_bot_secret
     raise NotAuthorizedError unless params[:secret] == @telegram_bot.secret
-  end
-
-  def handle_message(message)
-    chat_id = Integer message[:chat][:id]
-    text    = String  message[:text]
-
-    RestClient.post(
-      "https://api.telegram.org/bot#{@telegram_bot.api_token}/sendMessage",
-      chat_id: chat_id,
-      text:    "Message received: #{text}",
-    )
   end
 end
