78cd46187335e492666872cc22f4532114a19ea2
Alex Kotov <kotovalexarian@gmail.com>
2018-11-30 02:21:33 +0500

    Add Raven user context

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
 private
 
   def set_raven_context
+    Raven.user_context id: current_user.id if user_signed_in?
     Raven.extra_context params: params.to_unsafe_h, url: request.url
   end
 
