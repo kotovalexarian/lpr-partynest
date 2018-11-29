1ce8d038e50030224fc72c91bd63f5aadbda6754
Alex Kotov <kotovalexarian@gmail.com>
2018-11-29 17:37:31 +0500

    Fix code style

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,17 @@ Rails.application.routes.draw do
   get '/s/243898', to: 'home#post1'
   get '/s/377295', to: 'home#post2'
 
-  get '/events/2018/11/24/otkrytaja-vstrecha-storonnikov-libertarianskoj-partii.html' => 'home#event1'
-  get '/posts/2018/07/27/sformirovan-rukovodjashhij-komitet-ro-lpr-v-permskom-krae.html' => 'home#post1'
-  get '/posts/2018/11/12/sekretar-ro-lpr-v-permskom-krae-byl-zaderzhan-na-4-chasa.html' => 'home#post2'
+  get '/events/2018/11/24/' \
+      'otkrytaja-vstrecha-storonnikov-libertarianskoj-partii.html',
+      to: 'home#event1'
+
+  get '/posts/2018/07/27/' \
+      'sformirovan-rukovodjashhij-komitet-ro-lpr-v-permskom-krae.html',
+      to: 'home#post1'
+
+  get '/posts/2018/11/12/' \
+      'sekretar-ro-lpr-v-permskom-krae-byl-zaderzhan-na-4-chasa.html',
+      to: 'home#post2'
 
   resources :membership_applications, only: %i[new create]
 end
