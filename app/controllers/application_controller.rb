ee431d86d12543822bcad4c6947bdcb182d37aa1
Alex Kotov <kotovalexarian@gmail.com>
2018-11-29 18:14:08 +0500

    Do not verify authenticity token for JSON requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token, if: :json_request?
+
+private
+
+  def json_request?
+    request.format.json?
+  end
 end
