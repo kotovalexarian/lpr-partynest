cba8af05a7058498b9ed87bc4b6f6c37ebb83ff3
Alex Kotov <kotovalexarian@gmail.com>
2018-11-29 18:25:12 +0500

    Remove empty lines

--- db/migrate/20181126132402_create_membership_applications.rb
+++ db/migrate/20181126132402_create_membership_applications.rb
@@ -4,7 +4,6 @@ class CreateMembershipApplications < ActiveRecord::Migration[5.2]
   def change
     create_table :membership_applications do |t|
       t.timestamps null: false
-
       t.string :first_name, null: false
       t.string :last_name, null: false
       t.string :middle_name
--- db/migrate/20181129131751_create_telegram_bots.rb
+++ db/migrate/20181129131751_create_telegram_bots.rb
@@ -4,7 +4,6 @@ class CreateTelegramBots < ActiveRecord::Migration[5.2]
   def change
     create_table :telegram_bots do |t|
       t.timestamps null: false
-
       t.string :secret, null: false
     end
   end
