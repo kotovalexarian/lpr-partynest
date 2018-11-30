8f7e316ee026dcac1127ae2470a20a31e6d34c5d
Alex Kotov <kotovalexarian@gmail.com>
2018-11-30 08:41:57 +0500

    Factory generates Passport#image

--- factories/passports.rb
+++ factories/passports.rb
@@ -2,6 +2,15 @@
 
 FactoryBot.define do
   factory :passport do
+    transient do
+      image_filename { image_fixture }
+      image_fixture { "passport_image_#{rand(1..4)}.jpg" }
+
+      image_path do
+        File.join File.expand_path('..', __dir__), 'fixtures', image_fixture
+      end
+    end
+
     surname { Faker::Name.last_name }
     given_name { Faker::Name.first_name }
     patronymic { Faker::Name.first_name }
@@ -15,5 +24,12 @@ FactoryBot.define do
       "#{rand(0..999).to_s.rjust(3, '0')}-#{rand(0..999).to_s.rjust(3, '0')}"
     end
     date_of_issue { Faker::Date.backward }
+
+    after :create do |passport, evaluator|
+      passport.image.attach(
+        filename: evaluator.image_filename,
+        io:       File.open(evaluator.image_path),
+      )
+    end
   end
 end
