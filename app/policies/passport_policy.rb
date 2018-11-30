369d01f65042e38177b4497dac11ae9007e86e2c
Alex Kotov <kotovalexarian@gmail.com>
2018-11-30 08:51:30 +0500

    Configure permitted attributes

--- app/policies/passport_policy.rb
+++ app/policies/passport_policy.rb
@@ -5,6 +5,17 @@ class PassportPolicy < ApplicationPolicy
     true
   end
 
+  def permitted_attributes_for_create
+    %i[
+      surname given_name patronymic sex date_of_birth place_of_birth series
+      number issued_by unit_code date_of_issue
+    ]
+  end
+
+  def permitted_attributes_for_update
+    %i[image]
+  end
+
   class Scope < Scope
     def resolve
       scope.all
