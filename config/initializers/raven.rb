f45c5bbe8bbd9fdbd0f69b3406d20881209d6452
Alex Kotov <kotovalexarian@gmail.com>
2018-11-30 12:43:44 +0500

    Only enable Raven in production environment

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -3,6 +3,7 @@
 # Be sure to restart your server when you modify this file.
 
 Raven.configure do |config|
+  config.environments = %i[production]
   config.dsn = Rails.application.credentials.raven_dsn
   config.sanitize_fields = Partynest::Application::FILTER_PARAMS.map(&:to_s)
 end
