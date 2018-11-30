e32756ab51d49f7690daa7d877cb1a9cbdd013f7
Alex Kotov <kotovalexarian@gmail.com>
2018-11-30 11:58:27 +0500

    Add method MembershipApplicationPolicy#permitted_attributes_for_create

--- app/controllers/membership_applications_controller.rb
+++ app/controllers/membership_applications_controller.rb
@@ -1,19 +1,6 @@
 # frozen_string_literal: true
 
 class MembershipApplicationsController < ApplicationController
-  CREATE_ATTRIBUTES = %i[
-    first_name
-    last_name
-    middle_name
-    date_of_birth
-    occupation
-    email
-    phone_number
-    telegram_username
-    organization_membership
-    comment
-  ].freeze
-
   # GET /membership_applications/new
   def new
     @membership_application = MembershipApplication.new
@@ -23,18 +10,13 @@ class MembershipApplicationsController < ApplicationController
 
   # POST /membership_applications
   def create
-    @membership_application = MembershipApplication.new create_params
+    @membership_application =
+      MembershipApplication.new permitted_attributes MembershipApplication
 
     authorize @membership_application
 
     return render :new unless @membership_application.save
 
     redirect_to root_url
-  end
-
-private
-
-  def create_params
-    params.require(:membership_application).permit(CREATE_ATTRIBUTES)
   end
 end
--- app/policies/membership_application_policy.rb
+++ app/policies/membership_application_policy.rb
@@ -4,4 +4,11 @@ class MembershipApplicationPolicy < ApplicationPolicy
   def create?
     true
   end
+
+  def permitted_attributes_for_create
+    %i[
+      first_name last_name middle_name date_of_birth occupation email
+      phone_number telegram_username organization_membership comment
+    ]
+  end
 end
