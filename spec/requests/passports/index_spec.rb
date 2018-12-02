49c5ed29dd1de02b4d980513ca485eca92c66c62
Alex Kotov <kotovalexarian@gmail.com>
2018-12-02 22:29:09 +0500

    Fix error

--- spec/requests/passports/index_spec.rb
+++ spec/requests/passports/index_spec.rb
@@ -4,6 +4,14 @@ require 'rails_helper'
 
 RSpec.describe 'GET /passports' do
   before do
+    create :empty_passport
+    create :passport_with_passport_map
+    create :passport_with_image
+    create :passport_with_passport_map_and_image
+    create :passport_with_almost_enough_confirmations
+    create :passport_with_enough_confirmations
+    create :confirmed_passport
+
     get '/passports'
   end
 
