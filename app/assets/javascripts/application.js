a34941f515197bf6956b06406c32fa4aa157d02b
Alex Kotov <kotovalexarian@gmail.com>
2018-12-02 18:20:22 +0500

    Install Viewer.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,4 +4,12 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require viewerjs/dist/viewer
+//= require jquery-viewer/dist/jquery-viewer
 //= require_tree .
+
+$(document).ready(function() {
+  $('#image').viewer({
+    inline: true,
+  })
+})
