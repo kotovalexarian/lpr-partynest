3c103699565b32c7144f94de2d480dcc4e6cb3cb
Alex Kotov <kotovalexarian@gmail.com>
2018-12-04 00:24:19 +0500

    Fix typo

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -22,7 +22,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = Rails.application.credentials.domain
+  config.mailer_sender = "no-reply@#{Rails.application.credentials.domain}"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
