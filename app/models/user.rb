9e5582f4d5974573e9c97042144d58469399df72
Alex Kotov <kotovalexarian@gmail.com>
2018-12-04 01:19:41 +0500

    Enable Devise lockable

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise(
     :confirmable,
     :database_authenticatable,
-    # :lockable,
+    :lockable,
     # :omniauthable,
     :recoverable,
     :registerable,
