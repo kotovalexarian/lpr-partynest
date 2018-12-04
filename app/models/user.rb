623aff5f941a9dcdc64cf84caff6eba65b75126d
Alex Kotov <kotovalexarian@gmail.com>
2018-12-04 09:15:44 +0500

    Revert "Allow unconfirmed access for 3 days when OmniAuth sign up"
    This reverts commit 1eac19662f6d086e1258bb96b83f672a50724da1.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  ALLOW_UNCONFIRMED_OMNIAUTH_ACCESS_FOR = 3.days
-
   devise(
     :confirmable,
     :database_authenticatable,
@@ -25,14 +23,5 @@ class User < ApplicationRecord
 
   before_validation do
     self.account ||= Account.new
-  end
-
-  def confirmation_period_valid?
-    return if user_omniauths.where(email: email).exists? &&
-              confirmation_sent_at &&
-              confirmation_sent_at.utc >=
-              ALLOW_UNCONFIRMED_OMNIAUTH_ACCESS_FOR.ago
-
-    super
   end
 end
