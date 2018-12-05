be4da1272e2e9d3abe6883d94a31aab7d077e5a5
Alex Kotov <kotovalexarian@gmail.com>
2018-12-06 04:42:29 +0500

    Remove unnecessary comments

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,18 +27,10 @@ set :repo_url, 'git@github.com:libertarian-party/partynest.git'
 # set :pty, true
 
 # Default value for :linked_files is [].
-# append :linked_files,
-#        'config/database.yml'
 append :linked_files,
        'config/master.key'
 
 # Default value for linked_dirs is [].
-# append :linked_dirs,
-#        'log',
-#        'tmp/pids',
-#        'tmp/cache',
-#        'tmp/sockets',
-#        'public/system'
 append :linked_dirs,
        'log',
        'storage',
