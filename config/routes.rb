5848752950b5d942abcb6f637770057cafa4c3b4
Alex Kotov <kotovalexarian@gmail.com>
2018-12-05 23:08:07 +0500

    Remove old content

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/s/452465', to: 'home#event1'
-  get '/s/243898', to: 'home#post1'
-  get '/s/377295', to: 'home#post2'
-
-  get '/events/2018/11/24/' \
-      'otkrytaja-vstrecha-storonnikov-libertarianskoj-partii.html',
-      to: 'home#event1'
-
-  get '/posts/2018/07/27/' \
-      'sformirovan-rukovodjashhij-komitet-ro-lpr-v-permskom-krae.html',
-      to: 'home#post1'
-
-  get '/posts/2018/11/12/' \
-      'sekretar-ro-lpr-v-permskom-krae-byl-zaderzhan-na-4-chasa.html',
-      to: 'home#post2'
-
-  #####
-
   root to: 'home#show'
 
   devise_for :users, controllers: {
