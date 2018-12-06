64ef47b82afb37da887e341d7938dc5a0093af87
Alex Kotov <kotovalexarian@gmail.com>
2018-12-06 23:16:20 +0500

    Add UserMailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class UserMailer < Devise::Mailer
+  # def confirmation_instructions(record, token, opts = {})
+  #   super
+  # end
+
+  # def reset_password_instructions(record, token, opts = {})
+  #   super
+  # end
+
+  # def unlock_instructions(record, token, opts = {})
+  #   super
+  # end
+
+  # def email_changed(record, opts = {})
+  #   super
+  # end
+
+  # def password_change(record, opts = {})
+  #   super
+  # end
+end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -23,7 +23,7 @@ Devise.setup do |config|
   config.mailer_sender = Rails.application.config.noreply_email_contact
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'UserMailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
