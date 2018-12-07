52f15e06137c2e18017b49a975a79382ce6a9fdc
Alex Kotov <kotovalexarian@gmail.com>
2018-12-07 07:24:33 +0500

    Set Telegram Bot API webhook

--- app/interactors/initialize_telegram_bot.rb
+++ app/interactors/initialize_telegram_bot.rb
@@ -3,12 +3,47 @@
 class InitializeTelegramBot
   include Interactor
 
+  include Rails.application.routes.url_helpers
+
   def call
-    url = "https://api.telegram.org/bot#{context.telegram_bot.api_token}/getMe"
-    data = JSON.parse RestClient.get(url).body
+    fetch_info
+    set_webhook
+  end
+
+private
+
+  def fetch_info
+    data = call_api_method_get :getMe
 
     context.fail! unless data['ok'] && context.telegram_bot.update(
       username: data['result']['username'],
+    )
+  end
+
+  def set_webhook
+    data = call_api_method_get(
+      :setWebhook,
+      max_connections: 1,
+      url:             CGI.escape(webhook_url),
+    )
+
+    context.fail! unless data['ok']
+  end
+
+  def api_method_url(method)
+    "https://api.telegram.org/bot#{context.telegram_bot.api_token}/#{method}"
+  end
+
+  def call_api_method_get(method, params = {})
+    JSON.parse RestClient.get(api_method_url(method), params).body
+  end
+
+  def webhook_url
+    telegram_bot_updates_url(
+      format:   'json',
+      protocol: 'https',
+      host:     Rails.application.config.site_domain,
+      secret:   context.telegram_bot.secret,
     )
   end
 end
