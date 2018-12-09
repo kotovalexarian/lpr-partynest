e10248edd5ba6b9d0ac492fe966eba6edd91a711
Alex Kotov <kotovalexarian@gmail.com>
2018-12-09 08:10:22 +0500

    Fix code style

--- config/initializers/action_mailer_smtp_settings.rb
+++ config/initializers/action_mailer_smtp_settings.rb
@@ -2,15 +2,15 @@
 
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config_for(:smtp).deep_symbolize_keys.tap do |config|
-  ActionMailer::Base.smtp_settings = {
-    address:              config[:address],
-    port:                 config[:port]&.to_i,
-    domain:               config[:domain],
-    user_name:            config[:user_name],
-    password:             config[:password],
-    authentication:       config[:authentication]&.to_sym,
-    enable_starttls_auto: config[:enable_starttls_auto],
-    openssl_verify_mode:  config[:openssl_verify_mode],
-  }
-end
+conf = Rails.application.config_for(:smtp).deep_symbolize_keys
+
+ActionMailer::Base.smtp_settings = {
+  address:              conf[:address],
+  port:                 conf[:port]&.to_i,
+  domain:               conf[:domain],
+  user_name:            conf[:user_name],
+  password:             conf[:password],
+  authentication:       conf[:authentication]&.to_sym,
+  enable_starttls_auto: conf[:enable_starttls_auto],
+  openssl_verify_mode:  conf[:openssl_verify_mode],
+}
