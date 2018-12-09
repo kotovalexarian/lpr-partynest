bc908c264a1ec46fbf4b76f7dd7b7394dde1d861
Alex Kotov <kotovalexarian@gmail.com>
2018-12-09 09:39:44 +0500

    Auto restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,3 +50,7 @@ append :linked_dirs,
 # Uncomment the following to require manually verifying the host key
 # before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+after 'deploy:publishing', 'deploy:restart'
+
+after 'deploy:restart', 'puma:restart'
