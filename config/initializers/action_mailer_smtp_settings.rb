2e54a159f89f44de02630b9f9291799d838c7bd9
Alex Kotov <kotovalexarian@gmail.com>
2018-12-09 08:06:27 +0500

    Add file "config/smtp.yml"

--- config/initializers/action_mailer_smtp_settings.rb
+++ config/initializers/action_mailer_smtp_settings.rb
@@ -2,12 +2,15 @@
 
 # Be sure to restart your server when you modify this file.
 
-ActionMailer::Base.smtp_settings = {
-  user_name:            'apikey',
-  password:             Rails.application.credentials.sendgrid_api_key,
-  domain:               Rails.application.config.site_domain,
-  address:              'smtp.sendgrid.net',
-  port:                 587,
-  authentication:       :plain,
-  enable_starttls_auto: true,
-}
+Rails.application.config_for(:smtp).try(&:deep_symbolize_keys).try do |config|
+  ActionMailer::Base.smtp_settings = {
+    address:              config[:address],
+    port:                 config[:port]&.to_i,
+    domain:               config[:domain],
+    user_name:            config[:user_name],
+    password:             config[:password],
+    authentication:       config[:authentication]&.to_sym,
+    enable_starttls_auto: config[:enable_starttls_auto],
+    openssl_verify_mode:  config[:openssl_verify_mode],
+  }
+end
