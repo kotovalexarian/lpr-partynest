9781908a230376aa4082fa9b2598df97b9d3c96d
Alex Kotov <kotovalexarian@gmail.com>
2018-12-09 07:49:42 +0500

    Reorder code

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,17 +2,8 @@
 
 # Be sure to restart your server when you modify this file.
 
-server_conf = Rails.application.config_for(:sidekiq_server).deep_symbolize_keys
 client_conf = Rails.application.config_for(:sidekiq_client).deep_symbolize_keys
-
-Sidekiq.configure_server do |config|
-  config.redis = {
-    host:     server_conf[:redis_host],
-    port:     server_conf[:redis_port],
-    db:       server_conf[:redis_db],
-    password: server_conf[:redis_password],
-  }
-end
+server_conf = Rails.application.config_for(:sidekiq_server).deep_symbolize_keys
 
 Sidekiq.configure_client do |config|
   config.redis = {
@@ -20,5 +11,14 @@ Sidekiq.configure_client do |config|
     port:     client_conf[:redis_port],
     db:       client_conf[:redis_db],
     password: client_conf[:redis_password],
+  }
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = {
+    host:     server_conf[:redis_host],
+    port:     server_conf[:redis_port],
+    db:       server_conf[:redis_db],
+    password: server_conf[:redis_password],
   }
 end
