13c6934d77f3590bad1ef12a1dbc6c24a2711266
Alex Kotov <kotovalexarian@gmail.com>
2018-12-10 18:04:25 +0500

    Reorder code

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -3,7 +3,7 @@
 # Be sure to restart your server when you modify this file.
 
 Raven.configure do |config|
-  config.environments = %w[production]
   config.dsn = Rails.application.credentials.raven_dsn
+  config.environments = %w[production]
   config.sanitize_fields = Partynest::Application::FILTER_PARAMS.map(&:to_s)
 end
