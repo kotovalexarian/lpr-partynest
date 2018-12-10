d8265693c4371329ea4b07dd18e11bd2ae6d9a38
Alex Kotov <kotovalexarian@gmail.com>
2018-12-10 18:06:11 +0500

    Configure Sentry Raven excluded exceptions

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -6,5 +6,16 @@ Raven.configure do |config|
   config.current_environment = Rails.env
   config.dsn = Rails.application.credentials.raven_dsn
   config.environments = %w[production]
+  config.excluded_exceptions = %w[
+    AbstractController::ActionNotFound
+    ActionController::InvalidAuthenticityToken
+    ActionController::RoutingError
+    ActionController::UnknownAction
+    ActiveRecord::RecordNotFound
+    CGI::Session::CookieStore::TamperedWithCookie
+    Mongoid::Errors::DocumentNotFound
+    Sinatra::NotFound
+    ActiveJob::DeserializationError
+  ]
   config.sanitize_fields = Partynest::Application::FILTER_PARAMS.map(&:to_s)
 end
