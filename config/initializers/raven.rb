b419ceecf421823f9df62742a3e452a8a812abc8
Alex Kotov <kotovalexarian@gmail.com>
2018-12-10 18:04:46 +0500

    Set Sentry Raven environment

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -3,6 +3,7 @@
 # Be sure to restart your server when you modify this file.
 
 Raven.configure do |config|
+  config.current_environment = Rails.env
   config.dsn = Rails.application.credentials.raven_dsn
   config.environments = %w[production]
   config.sanitize_fields = Partynest::Application::FILTER_PARAMS.map(&:to_s)
