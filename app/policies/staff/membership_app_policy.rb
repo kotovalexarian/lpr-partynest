9b92be20c24ae88643075a65294afe4d5498e6f1
Alex Kotov <kotovalexarian@gmail.com>
2018-12-13 08:44:50 +0500

    Add Staff::MembershipAppPolicy

---
+++ app/policies/staff/membership_app_policy.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Staff::MembershipAppPolicy < ApplicationPolicy
+  def index?
+    account&.is_superuser?
+  end
+
+  def show?
+    account&.is_superuser?
+  end
+
+  class Scope < Scope
+    def resolve
+      return scope.all if account&.is_superuser?
+
+      scope.none
+    end
+  end
+end
---
+++ spec/policies/staff/membership_app_policy_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Staff::MembershipAppPolicy do
+  subject { described_class.new account, record }
+
+  let :resolved_scope do
+    described_class::Scope.new(account, MembershipApp.all).resolve
+  end
+
+  let(:record) { create :membership_app }
+
+  before { create_list :membership_app, 3 }
+
+  [
+    nil,
+    :guest_account,
+    :usual_account,
+  ].each do |account_type|
+    context "when #{account_type || :no_account} is authenticated" do
+      let(:account) { create account_type if account_type }
+
+      it { is_expected.to forbid_actions %i[index show destroy] }
+      it { is_expected.to forbid_new_and_create_actions }
+      it { is_expected.to forbid_edit_and_update_actions }
+
+      specify { expect(resolved_scope).to be_empty }
+    end
+  end
+
+  context 'when superuser account is authenticated' do
+    let(:account) { create :superuser_account }
+
+    it { is_expected.to permit_actions %i[index show] }
+
+    it { is_expected.to forbid_new_and_create_actions }
+    it { is_expected.to forbid_edit_and_update_actions }
+    it { is_expected.to forbid_action :destroy }
+
+    specify { expect(resolved_scope).to eq MembershipApp.all }
+  end
+end
