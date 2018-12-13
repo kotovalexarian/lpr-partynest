70ed004c8ea01a9e8f529cf434448c27ad775582
Alex Kotov <kotovalexarian@gmail.com>
2018-12-13 09:31:27 +0500

    Improve code

--- lib/partynest/rspec_account_role_helpers.rb
+++ lib/partynest/rspec_account_role_helpers.rb
@@ -2,28 +2,32 @@
 
 module Partynest
   module RSpecAccountRoleHelpers
-    def for_account_types(*account_types, &block)
-      account_types.each do |account_type|
-        account_type = :"#{account_type}_account" unless account_type.nil?
+    def create_account_type(account_type)
+      return if account_type.nil?
 
-        context "when #{account_type || :no_account} is authenticated" do
-          let(:current_account) { create account_type if account_type }
-          let(:account) { current_account }
-
-          instance_eval(&block)
-        end
-      end
+      create "#{account_type}_account"
     end
 
-    def xfor_account_types(*account_types, &block)
-      account_types.each do |account_type|
-        account_type = :"#{account_type}_account" unless account_type.nil?
+    module ClassMethods
+      def for_account_types(*account_types, &block)
+        account_types.each do |account_type|
+          context "when #{account_type || :no_account} is authenticated" do
+            let(:current_account) { create_account_type account_type }
+            let(:account) { current_account }
 
-        xcontext "when #{account_type || :no_account} is authenticated" do
-          let(:current_account) { create account_type if account_type }
-          let(:account) { current_account }
+            instance_eval(&block)
+          end
+        end
+      end
 
-          instance_eval(&block)
+      def xfor_account_types(*account_types, &block)
+        account_types.each do |account_type|
+          xcontext "when #{account_type || :no_account} is authenticated" do
+            let(:current_account) { create_account_type account_type }
+            let(:account) { current_account }
+
+            instance_eval(&block)
+          end
         end
       end
     end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -75,5 +75,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.extend Partynest::RSpecAccountRoleHelpers
+  config.include Partynest::RSpecAccountRoleHelpers
+  config.extend  Partynest::RSpecAccountRoleHelpers::ClassMethods
 end
