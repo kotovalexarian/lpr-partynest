6be30d2d7f000ed20b87363e282629960a5a47ab
Alex Kotov <kotovalexarian@gmail.com>
2019-01-31 06:14:30 +0500

    Remove unnecessary Capistrano tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,8 +50,3 @@ append :linked_dirs,
 # Uncomment the following to require manually verifying the host key
 # before first deploy.
 # set :ssh_options, verify_host_key: :secure
-
-after 'deploy:publishing', 'deploy:restart'
-
-after 'deploy:restart', 'puma:stop'
-after 'deploy:restart', 'sidekiq:stop'
