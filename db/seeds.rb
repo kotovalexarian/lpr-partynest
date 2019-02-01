87a4732823386475cb11dcf9ac57842729b92531
Alex Kotov <kotovalexarian@gmail.com>
2019-02-02 04:09:59 +0500

    Seed initial superuser account username, public name and bio

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,8 +12,12 @@ end
 
 Rails.application.settings(:superuser).tap do |config|
   User.where(email: config[:email]).first_or_create! do |new_user|
-    new_user.account = Account.create!
     new_user.password = config[:password]
     new_user.confirmed_at = Time.zone.now
+    new_user.account = Account.create!(
+      username:    config[:username],
+      public_name: config[:public_name],
+      biography:   config[:biography],
+    )
   end.account.add_role :superuser
 end
