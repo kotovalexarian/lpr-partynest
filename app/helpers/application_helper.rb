c314d59cad6eeb357621d585f12d1929dd538976
Alex Kotov <kotovalexarian@gmail.com>
2019-03-27 05:32:35 +0500

    Add ApplicationHelper#nav_tabs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  NAV_TABS_LIST_HTML_CLASS = {
+    tabs:  'nav nav-tabs  d-none d-sm-flex',
+    pills: 'nav nav-pills d-flex d-sm-none flex-column',
+  }.freeze
+
   def bootstrap_class_for_flash(flash_type)
     case flash_type
     when 'success'
@@ -11,6 +16,29 @@ module ApplicationHelper
       'alert-warning'
     else
       'alert-info'
+    end
+  end
+
+  def nav_tabs(scope_name, current_tab_name, options)
+    capture do
+      concat nav_tabs_list scope_name, current_tab_name, :tabs, options
+      concat nav_tabs_list scope_name, current_tab_name, :pills, options
+    end
+  end
+
+  def nav_tabs_list(scope_name, current_tab_name, format, options)
+    tag.ul class: NAV_TABS_LIST_HTML_CLASS[format] do
+      options.each do |(k, v)|
+        concat nav_tabs_item scope_name, current_tab_name, k, v
+      end
+    end
+  end
+
+  def nav_tabs_item(scope_name, current_tab_name, tab_name, url)
+    tag.li class: 'nav-item' do
+      link_to translate(tab_name, scope: [:nav_tabs, scope_name]),
+              url,
+              class: "nav-link #{:active if current_tab_name == tab_name}"
     end
   end
 end
