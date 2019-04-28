bde61d24fc3ad4903e54f3fe68cbf7e48313af63
Alex Kotov <kotovalexarian@gmail.com>
2019-04-28 19:34:09 +0500

    Reorder code

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -39,6 +39,23 @@ class Relationship < ApplicationRecord
     end
   end
 
+private
+
+  def membership_dates_are_not_in_future
+    errors.add :supporter_since unless supporter_since_not_in_future?
+    errors.add :member_since    unless member_since_not_in_future?
+    errors.add :excluded_since  unless excluded_since_not_in_future?
+  end
+
+  def membership_is_possible
+    errors.add :member_since unless member_since_not_before_supporter_since?
+
+    return if excluded_since_not_before_member_since? &&
+              excluded_since_not_before_supporter_since?
+
+    errors.add :excluded_since
+  end
+
   def supporter_since_not_in_future?
     return true if supporter_since.nil?
 
@@ -73,22 +90,5 @@ class Relationship < ApplicationRecord
     return true if excluded_since.nil? || member_since.nil?
 
     excluded_since >= member_since
-  end
-
-private
-
-  def membership_dates_are_not_in_future
-    errors.add :supporter_since unless supporter_since_not_in_future?
-    errors.add :member_since    unless member_since_not_in_future?
-    errors.add :excluded_since  unless excluded_since_not_in_future?
-  end
-
-  def membership_is_possible
-    errors.add :member_since unless member_since_not_before_supporter_since?
-
-    return if excluded_since_not_before_member_since? &&
-              excluded_since_not_before_supporter_since?
-
-    errors.add :excluded_since
   end
 end
