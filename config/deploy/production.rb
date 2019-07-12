4c60b50bb6ae0aee99b34a7e48714ab06c3b12f8
Alex Kotov <kotovalexarian@gmail.com>
2019-07-12 21:15:01 +0500

    Remove unnecessary deployment code

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,15 +1,3 @@
 # frozen_string_literal: true
 
-server '138.197.0.6', roles: %w[web app db]
-
-namespace :deploy do
-  after :published, :restart
-
-  desc 'Restart application services'
-  task :restart do
-    on roles(:app) do
-      execute :sudo, :systemctl, :restart, 'partynest-web.service'
-      execute :sudo, :systemctl, :restart, 'partynest-worker.service'
-    end
-  end
-end
+server 'partynest.libertarian-party.com', roles: %w[web app db]
