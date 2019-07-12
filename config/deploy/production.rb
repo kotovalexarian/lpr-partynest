a4ac9ff102d8b4db2487cbb52cb629f9727d6ef7
Alex Kotov <kotovalexarian@gmail.com>
2019-07-12 22:42:04 +0500

    Change group

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,14 @@
 # frozen_string_literal: true
 
 server 'partynest.libertarian-party.com', roles: %w[web app db]
+
+namespace :deploy do
+  after :finished, :change_group
+
+  desc 'Change group'
+  task :change_group do
+    on roles(:all) do
+      execute :chown, '-R', ':partynest', '/opt/partynest'
+    end
+  end
+end
