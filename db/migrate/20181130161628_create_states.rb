7d7d8fe83670378ddc0ee1f6d263117c350170c3
Alex Kotov <kotovalexarian@gmail.com>
2019-07-20 04:45:03 +0500

    Merge migrations

--- db/migrate/20181130161628_create_states.rb
+++ db/migrate/20181130161628_create_states.rb
@@ -2,7 +2,7 @@
 
 class CreateStates < ActiveRecord::Migration[5.2]
   def change
-    create_table :states do |t|
+    create_table :country_states do |t|
       t.timestamps null: false
       t.string :name, null: false
 
--- db/migrate/20181130163121_rename_states_to_country_states.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RenameStatesToCountryStates < ActiveRecord::Migration[5.2]
-  def change
-    rename_table :states, :country_states
-  end
-end
