684f51b7b9b285978e84e7a47e8731fe0fb3409e
Alex Kotov <kotovalexarian@gmail.com>
2019-07-20 10:54:17 +0500

    Improve migration

--- db/migrate/20190427141639_create_relationships.rb
+++ db/migrate/20190427141639_create_relationships.rb
@@ -5,16 +5,17 @@ class CreateRelationships < ActiveRecord::Migration[6.0]
     create_table :relationships do |t|
       t.timestamps null: false
 
-      t.references :person,          null: false, index: false
-      t.references :regional_office, null: false, index: true, foreign_key: true
+      t.references :person,
+                   null: false, index: false, foreign_key: true
+
+      t.references :regional_office,
+                   null: false, index: true,  foreign_key: true
 
       t.integer :number,       null: false
       t.date    :active_since, null: false, index: true
       t.integer :status,       null: false, index: true
 
       t.index %i[person_id number], unique: true
-
-      t.foreign_key :people
     end
   end
 end
