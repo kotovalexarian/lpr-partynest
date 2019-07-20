289958669ab2c0e5d8f2c40829817c22e3c30998
Alex Kotov <kotovalexarian@gmail.com>
2019-07-20 05:07:50 +0500

    Remove unnecessary migrations

--- db/migrate/20181204024403_add_omniauth_to_users.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddOmniauthToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :omniauth_provider, :string
-    add_column :users, :omniauth_uid,      :string
-  end
-end
--- db/migrate/20181204030126_remove_omniauth_from_users.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveOmniauthFromUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :omniauth_provider, :string
-    remove_column :users, :omniauth_uid,      :string
-  end
-end
