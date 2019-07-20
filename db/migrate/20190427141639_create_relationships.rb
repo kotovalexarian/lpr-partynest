45b6dd709c2223fa1c1067ec89b9eb90d9a240c0
Alex Kotov <kotovalexarian@gmail.com>
2019-07-20 10:50:42 +0500

    Improve migration

--- db/migrate/20190427141639_create_relationships.rb
+++ db/migrate/20190427141639_create_relationships.rb
@@ -8,20 +8,13 @@ class CreateRelationships < ActiveRecord::Migration[6.0]
       t.references :person,          null: false, index: false
       t.references :regional_office, null: false, index: true, foreign_key: true
 
-      t.integer :number, null: false
+      t.integer :number,       null: false
+      t.date    :active_since, null: false, index: true
+      t.integer :status,       null: false, index: true
 
       t.index %i[person_id number], unique: true
+
       t.foreign_key :people
     end
-
-    # rubocop:disable Rails/NotNullColumn
-    add_column :relationships, :active_since, :date, null: false
-    # rubocop:enable Rails/NotNullColumn
-    add_index  :relationships, :active_since
-
-    # rubocop:disable Rails/NotNullColumn
-    add_column :relationships, :status, :integer, null: false
-    # rubocop:enable Rails/NotNullColumn
-    add_index  :relationships, :status
   end
 end
