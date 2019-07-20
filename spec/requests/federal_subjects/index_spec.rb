bad7ba56d3ae39a6bab6dd3db9ad6bead3ac4c3a
Alex Kotov <kotovalexarian@gmail.com>
2019-07-20 08:01:23 +0500

    Add specs for FederalSubjectsController#index

---
+++ spec/requests/federal_subjects/index_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'GET /federal_subjects' do
+  before do
+    sign_in current_account.user if current_account&.user
+
+    create_list :federal_subject, rand(1..3)
+
+    get '/federal_subjects'
+  end
+
+  for_account_types nil, :guest, :usual, :superuser do
+    specify do
+      expect(response).to have_http_status :ok
+    end
+  end
+end
