83846bd34ea7b449bb818d228a3cad88e3e47f29
Alex Kotov <kotovalexarian@gmail.com>
2019-07-26 03:35:33 +0500

    Import from raw CSV rows

--- app/interactors/import_person.rb
+++ app/interactors/import_person.rb
@@ -14,46 +14,57 @@ class ImportPerson
 private
 
   def create_person
-    context.person =
-      Person.where(id: context.person_id).lock(true).first_or_create!(
-        person_attributes.reverse_merge(
-          contacts_list: ContactsList.new,
-        ),
-      )
+    context.person = Person.where(id: person_id).lock(true).first_or_create!(
+      person_attributes.reverse_merge(
+        contacts_list: ContactsList.new,
+      ),
+    )
   end
 
   def create_general_comments_person_comment
-    return if context.general_comments.blank?
+    return if general_comments.blank?
 
     context.general_comments_person_comment =
       context.person.person_comments.where(origin: :general_comments)
       .lock(true).first_or_initialize
 
-    context.general_comments_person_comment.text = context.general_comments
+    context.general_comments_person_comment.text = general_comments
 
     context.general_comments_person_comment.save!
   end
 
   def create_first_contact_date_person_comment
-    return if context.first_contact_date.blank?
+    return if first_contact_date.blank?
 
     context.first_contact_date_person_comment =
       context.person.person_comments.where(origin: :first_contact_date)
       .lock(true).first_or_initialize
 
-    context.first_contact_date_person_comment.text = context.first_contact_date
+    context.first_contact_date_person_comment.text = first_contact_date
 
     context.first_contact_date_person_comment.save!
   end
 
+  def person_id
+    context.row[0]
+  end
+
   def person_attributes
     {
-      last_name: context.last_name,
-      first_name: context.first_name,
-      middle_name: context.middle_name,
-      sex: context.sex,
-      date_of_birth: context.date_of_birth,
-      place_of_birth: context.place_of_birth,
+      last_name: context.row[2],
+      first_name: context.row[1],
+      middle_name: context.row[3],
+      sex: nil,
+      date_of_birth: context.row[8],
+      place_of_birth: context.row[9],
     }
+  end
+
+  def general_comments
+    context.row[5]
+  end
+
+  def first_contact_date
+    context.row[6]
   end
 end
