520522d72a7a00a54944c557eda0a368fa4ee50f
Alex Kotov <kotovalexarian@gmail.com>
2019-07-26 06:24:30 +0500

    Fix error

--- app/interactors/import_person.rb
+++ app/interactors/import_person.rb
@@ -29,7 +29,9 @@ private
 
   # rubocop:disable Metrics/AbcSize
 
-  def create_passport
+  def create_passport # rubocop:disable Metrics/MethodLength
+    return if passport_attributes[:number].blank?
+
     context.passport =
       Passport
       .where(person_id: context.person.id).lock(true).first_or_initialize
@@ -137,84 +139,84 @@ private
   end
 
   def person_id
-    context.row[0]
+    context.row[0].presence
   end
 
   def region_id
-    context.row[14]
+    context.row[14].presence
   end
 
   def person_attributes
     {
-      last_name: context.row[2],
-      first_name: context.row[1],
-      middle_name: context.row[3],
+      last_name: context.row[2].presence,
+      first_name: context.row[1].presence,
+      middle_name: context.row[3].presence,
       sex: nil,
-      date_of_birth: context.row[8],
-      place_of_birth: context.row[9],
+      date_of_birth: context.row[8].presence,
+      place_of_birth: context.row[9].presence,
     }
   end
 
   def passport_attributes # rubocop:disable Metrics/MethodLength
     {
-      last_name: context.row[2],
-      first_name: context.row[1],
-      middle_name: context.row[3],
+      last_name: context.row[2].presence,
+      first_name: context.row[1].presence,
+      middle_name: context.row[3].presence,
       sex: :male,
-      date_of_birth: context.row[8],
-      place_of_birth: context.row[9],
+      date_of_birth: context.row[8].presence,
+      place_of_birth: context.row[9].presence,
 
-      series: context.row[38],
-      number: context.row[10],
-      issued_by: context.row[11],
-      unit_code: context.row[12],
-      date_of_issue: context.row[13],
-      zip_code: context.row[15],
+      series: context.row[38].presence,
+      number: context.row[10].presence,
+      issued_by: context.row[11].presence,
+      unit_code: context.row[12].presence,
+      date_of_issue: context.row[13].presence,
+      zip_code: context.row[15].presence,
 
-      town_type: context.row[20],
-      town_name: context.row[21],
-      settlement_type: context.row[22],
-      settlement_name: context.row[23],
-      district_type: context.row[18],
-      district_name: context.row[19],
-      street_type: context.row[16],
-      street_name: context.row[17],
-      residence_type: context.row[24],
-      residence_name: context.row[25],
-      building_type: context.row[26],
-      building_name: context.row[27],
-      apartment_type: context.row[28],
-      apartment_name: context.row[29],
+      town_type: context.row[20].presence,
+      town_name: context.row[21].presence,
+      settlement_type: context.row[22].presence,
+      settlement_name: context.row[23].presence,
+      district_type: context.row[18].presence,
+      district_name: context.row[19].presence,
+      street_type: context.row[16].presence,
+      street_name: context.row[17].presence,
+      residence_type: context.row[24].presence,
+      residence_name: context.row[25].presence,
+      building_type: context.row[26].presence,
+      building_name: context.row[27].presence,
+      apartment_type: context.row[28].presence,
+      apartment_name: context.row[29].presence,
     }
   end
 
   # rubocop:enable Metrics/AbcSize
 
   def general_comments
-    context.row[5]
+    context.row[5].presence
   end
 
   def first_contact_date
-    context.row[6]
+    context.row[6].presence
   end
 
   def latest_contact_date
-    context.row[7]
+    context.row[7].presence
   end
 
   def human_readable_id
-    context.row[34]
+    context.row[34].presence
   end
 
   def past_experience
-    context.row[35]
+    context.row[35].presence
   end
 
   def aid_at_2014_elections
-    context.row[36]
+    context.row[36].presence
   end
 
   def aid_at_2015_elections
-    context.row[37]
+    context.row[37].presence
   end
 end
