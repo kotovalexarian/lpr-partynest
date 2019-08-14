6d1f9679680313616359a701de3b3868ba588c6d
Alex Kotov <kotovalexarian@gmail.com>
2019-08-14 17:18:18 +0500

    Display Account#person

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 module PeopleHelper
+  def staff_person_link_or_none(person)
+    if person.nil?
+      translate :none
+    elsif policy([:staff, person]).show?
+      link_to person.full_name, [:staff, person]
+    else
+      person.full_name
+    end
+  end
+
   def person_status(person)
     if person.nil? || person.current_relationship.nil?
       return translate :not_in_party, scope: %i[helpers person_status]
