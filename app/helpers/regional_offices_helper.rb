3db59dee2f401a1515a81631cce137f6bf49d7e8
Alex Kotov <kotovalexarian@gmail.com>
2019-08-14 17:14:05 +0500

    Add helper method RegionalOfficesHelper#regional_office_link_or_none

---
+++ app/helpers/regional_offices_helper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module RegionalOfficesHelper
+  def regional_office_link_or_none(regional_office)
+    if regional_office.nil?
+      translate :none
+    elsif policy(regional_office.federal_subject).show?
+      link_to regional_office.federal_subject.display_name,
+              regional_office.federal_subject
+    else
+      regional_office.federal_subject.display_name
+    end
+  end
+end
