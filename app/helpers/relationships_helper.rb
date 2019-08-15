f701d1072c77a7d3171c87f867cff923ca48f783
Alex Kotov <kotovalexarian@gmail.com>
2019-08-15 08:05:22 +0500

    Add helper method RelationshipsHelper#relationship_from_date_or_none

--- app/helpers/relationships_helper.rb
+++ app/helpers/relationships_helper.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 module RelationshipsHelper
+  def relationship_from_date_or_none(relationship)
+    if relationship&.from_date.blank?
+      translate :none
+    else
+      localize relationship.from_date, format: :long
+    end
+  end
+
   def relationship_status_or_none(relationship)
     translate_enum :relationship_status, relationship&.status || false
   end
