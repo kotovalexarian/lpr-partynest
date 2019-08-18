b621243eb02c2a9d7f3ee2418f4ade2be7841b3b
Alex Kotov <kotovalexarian@gmail.com>
2019-08-18 10:29:07 +0500

    Implement interactor ImportRegionalOffice

--- app/interactors/import_regional_office.rb
+++ app/interactors/import_regional_office.rb
@@ -3,5 +3,27 @@
 class ImportRegionalOffice
   include Interactor
 
-  def call; end
+  def call
+    federal_subject = FederalSubject.find federal_subject_id
+
+    context.regional_office =
+      RegionalOffice.where(id: regional_office_id).first_or_create!(
+        federal_subject: federal_subject,
+        name: name,
+      )
+  end
+
+private
+
+  def regional_office_id
+    context.row[0].presence
+  end
+
+  def federal_subject_id
+    context.row[2].presence
+  end
+
+  def name
+    context.row[1].presence
+  end
 end
