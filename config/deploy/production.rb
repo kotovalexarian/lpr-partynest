57d55416d5bb1e39360cc21daf9ea14593f621e0
Alex Kotov <kotovalexarian@gmail.com>
2019-08-26 10:12:42 +0500

    Add server "partynest-staff.libertarian-party.com"

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
 
-server 'partynest.libertarian-party.com', roles: %w[web app db]
+server 'partynest.libertarian-party.com', roles: %w[web app]
+
+server 'partynest-staff.libertarian-party.com', roles: %w[web app db]
