96b7686a80699446da8c77c319aea94c3ac73057
Alex Kotov <kotovalexarian@gmail.com>
2019-09-03 00:31:56 +0500

    Fix factory

--- factories/contact_networks.rb
+++ factories/contact_networks.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :contact_network, class: ContactNetwork do
-    codename { Faker::Internet.username 3..36, %w[_] }
+    codename { Faker::Internet.unique.username 3..36, %w[_] }
     name { Faker::Company.unique.name }
   end
 end
