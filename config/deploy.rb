7b4985518fc18baf231d8ce8c2a63e37b88c9816
Alex Kotov <kotovalexarian@gmail.com>
2019-09-09 02:43:45 +0500

    Specify RVM gemset

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,3 +50,5 @@ append :linked_dirs,
 # Uncomment the following to require manually verifying the host key
 # before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+set :rvm_ruby_version, '2.6.2@partynest'
