e56191689353b19b951751d24a9a630a0971160b
Alex Kotov <kotovalexarian@gmail.com>
2019-09-10 03:56:22 +0500

    Rename keys

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,20 +7,20 @@ server_conf = Rails.application.settings :sidekiq_server
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    host: client_conf[:redis_host],
-    port: client_conf[:redis_port],
-    db: client_conf[:redis_db],
-    password: client_conf[:redis_password],
-    ssl_params: client_conf[:redis_ssl],
+    host: client_conf[:host],
+    port: client_conf[:port],
+    db: client_conf[:db],
+    password: client_conf[:password],
+    ssl_params: client_conf[:ssl],
   }
 end
 
 Sidekiq.configure_server do |config|
   config.redis = {
-    host: server_conf[:redis_host],
-    port: server_conf[:redis_port],
-    db: server_conf[:redis_db],
-    password: server_conf[:redis_password],
-    ssl_params: client_conf[:redis_ssl],
+    host: server_conf[:host],
+    port: server_conf[:port],
+    db: server_conf[:db],
+    password: server_conf[:password],
+    ssl_params: client_conf[:ssl],
   }
 end
