4db0be18bc8d8d180ec4de7f8a89c731cac996d7
Alex Kotov <kotovalexarian@gmail.com>
2019-09-11 13:17:24 +0500

    Merge migrations

---
+++ db/migrate/20190911081459_create_x509_tables.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+class CreateX509Tables < ActiveRecord::Migration[6.0]
+  include Partynest::Migration
+
+  def change
+    create_table :rsa_public_keys do |t|
+      t.timestamps null: false
+
+      t.text    :pem,  null: false
+      t.integer :bits, null: false
+
+      t.index :pem, unique: true
+    end
+
+    constraint :rsa_public_keys, :bits, <<~SQL
+      bits in (2048, 4096)
+    SQL
+
+    create_table :x509_certificate_requests do |t|
+      t.timestamps null: false
+
+      t.references :rsa_public_key, null: false, foreign_key: true
+
+      t.string :distinguished_name, null: false
+      t.text   :pem,                null: false
+    end
+
+    constraint :x509_certificate_requests, :distinguished_name, <<~SQL
+      is_good_big_text(distinguished_name)
+    SQL
+
+    create_table :x509_certificates do |t|
+      t.timestamps null: false
+
+      t.references :x509_certificate_request, null: true, foreign_key: true
+
+      t.text :pem, null: false
+
+      t.datetime :not_before
+      t.datetime :not_after
+    end
+  end
+end
