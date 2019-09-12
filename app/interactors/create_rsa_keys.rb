ae921e3cab5ba23ab2497138fa55670387f6dc45
Alex Kotov <kotovalexarian@gmail.com>
2019-09-12 10:13:07 +0500

    Fix code style

--- app/interactors/create_rsa_keys.rb
+++ app/interactors/create_rsa_keys.rb
@@ -6,18 +6,23 @@ class CreateRSAKeys
   BITS = 4096
 
   def call
-    context.public_key = RSAPublicKey.create! do |public_key|
-      pkey = OpenSSL::PKey::RSA.new BITS
+    context.public_key =
+      RSAPublicKey.create!(attributes, &:encrypt_private_key_pem)
+  end
 
-      public_key.bits = BITS
+private
 
-      public_key.sha1   = Digest::SHA1.hexdigest   pkey.public_key.to_der
-      public_key.sha256 = Digest::SHA256.hexdigest pkey.public_key.to_der
+  def attributes
+    pkey = OpenSSL::PKey::RSA.new BITS
 
-      public_key.public_key_pem  = pkey.public_key.to_pem.freeze
-      public_key.private_key_pem = pkey.to_pem.freeze
+    {
+      bits: BITS,
 
-      public_key.encrypt_private_key_pem
-    end
+      sha1: Digest::SHA1.hexdigest(pkey.public_key.to_der),
+      sha256: Digest::SHA256.hexdigest(pkey.public_key.to_der),
+
+      public_key_pem: pkey.public_key.to_pem.freeze,
+      private_key_pem: pkey.to_pem.freeze,
+    }
   end
 end
