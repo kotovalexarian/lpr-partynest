2ab04a014b8f3eaade8ad4fe94a71e73ff7ba019
Alex Kotov <kotovalexarian@gmail.com>
2019-09-12 05:34:57 +0500

    Fix code style

--- app/controllers/staffs/x509_certificates/private_keys_controller.rb
+++ app/controllers/staffs/x509_certificates/private_keys_controller.rb
@@ -3,24 +3,19 @@
 class Staffs::X509Certificates::PrivateKeysController < ApplicationController
   before_action :set_x509_certificate
   before_action :set_rsa_public_key
+  before_action :set_secret
 
   # GET /staff/x509_certificates/:x509_certificate_id/private_key
-  def show # rubocop:disable Metrics/MethodLength
+  def show
     authorize [:staff, X509Certificate,
                PublicKeyPrivateKey.new(@rsa_public_key)]
 
-    result = DecryptRSAPrivateKey.call(
-      public_key: @rsa_public_key,
-      private_key_pem_key: Base64.urlsafe_decode64(params[:private_key_secret]),
-    )
+    result = DecryptRSAPrivateKey.call public_key: @rsa_public_key,
+                                       private_key_pem_key: @secret
 
     respond_to do |format|
-      if result.success?
-        format.key do
-          send_data result.private_key_pem_cleartext, filename: 'private.key'
-        end
-      else
-        format.key { render status: :unprocessable_entity, plain: '' }
+      format.key do
+        send_data result.private_key_pem_cleartext, filename: 'private.key'
       end
     end
   end
@@ -33,5 +28,9 @@ private
 
   def set_rsa_public_key
     @rsa_public_key = @x509_certificate.rsa_public_key
+  end
+
+  def set_secret
+    @secret = Base64.urlsafe_decode64 params[:private_key_secret]
   end
 end
