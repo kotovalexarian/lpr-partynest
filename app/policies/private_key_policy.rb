eae61994be1f50fd45cd6647fac27e226fe687af
Alex Kotov <kotovalexarian@gmail.com>
2019-09-15 04:35:05 +0500

    Only display alert to key owners and superusers

--- app/policies/private_key_policy.rb
+++ app/policies/private_key_policy.rb
@@ -2,10 +2,13 @@
 
 class PrivateKeyPolicy < ApplicationPolicy
   def show?
+    show_alert? && record.exist?
+  end
+
+  def show_alert?
     return false if account.nil?
 
-    (account.superuser? || account == record.account) &&
-      record.exist? &&
-      params[:private_key_pem_secret].present?
+    params[:private_key_pem_secret].present? &&
+      (account.superuser? || account == record.account)
   end
 end
