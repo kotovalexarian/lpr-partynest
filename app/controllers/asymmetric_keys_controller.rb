be9156a5da8cee0eb62fa092837aff3a60165e0f
Alex Kotov <kotovalexarian@gmail.com>
2019-09-15 04:03:50 +0500

    Move action PublicKeysController#show to AsymmetricKeysController#show

--- app/controllers/asymmetric_keys_controller.rb
+++ app/controllers/asymmetric_keys_controller.rb
@@ -12,6 +12,13 @@ class AsymmetricKeysController < ApplicationController
   # GET /asymmetric_keys/:id
   def show
     authorize @asymmetric_key
+
+    respond_to do |format|
+      format.html
+      format.pem do
+        send_data @asymmetric_key.public_key_pem, filename: 'public.pem'
+      end
+    end
   end
 
 private
