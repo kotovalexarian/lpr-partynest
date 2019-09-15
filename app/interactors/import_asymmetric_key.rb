c11bcc84d8664aa64fec0a0173a85c7e80cb32f5
Alex Kotov <kotovalexarian@gmail.com>
2019-09-15 15:13:52 +0500

    Add some consts

--- app/interactors/import_asymmetric_key.rb
+++ app/interactors/import_asymmetric_key.rb
@@ -3,5 +3,8 @@
 class ImportAsymmetricKey
   include Interactor
 
+  RSA_BITS_RE = /^RSA Public-Key: \((\d+) bit\)$/.freeze
+  ECURVE_CURVE_RE = /^ASN1 OID: ((-|\w)+)$/.freeze
+
   def call; end
 end
