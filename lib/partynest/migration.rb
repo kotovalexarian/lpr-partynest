b9772912acfea0493b409aa0dc42607eda8e0ae9
Alex Kotov <kotovalexarian@gmail.com>
2019-09-21 23:17:54 +0500

    Improve migration library

--- lib/partynest/migration.rb
+++ lib/partynest/migration.rb
@@ -4,34 +4,65 @@ module Partynest
   module Migration
     def func(name, sql)
       reversible do |dir|
-        dir.up { execute "CREATE FUNCTION #{name} #{sql}" }
-
-        dir.down { execute "DROP FUNCTION #{name}" }
+        dir.up   { func_creation(name, sql).call }
+        dir.down { func_deletion(name).call      }
       end
     end
 
     def enum(name, values)
       reversible do |dir|
-        dir.up do
-          execute <<~SQL
-            CREATE TYPE #{name}
-              AS ENUM (#{values.map { |s| "'#{s}'" }.join(', ')})
-          SQL
-        end
-
-        dir.down { execute "DROP TYPE #{name}" }
+        dir.up   { enum_creation(name, values).call }
+        dir.down { enum_deletion(name).call         }
       end
     end
 
     def constraint(table, name, check)
       reversible do |dir|
-        dir.up do
-          execute <<~SQL
-            ALTER TABLE #{table} ADD CONSTRAINT #{name} CHECK (#{check})
-          SQL
-        end
+        dir.up   { constraint_creation(table, name, check).call }
+        dir.down { constraint_deletion(table, name).call        }
+      end
+    end
 
-        dir.down { execute "ALTER TABLE #{table} DROP CONSTRAINT #{name}" }
+  private
+
+    def func_creation(name, sql)
+      lambda do
+        execute "CREATE FUNCTION #{name} #{sql}"
+      end
+    end
+
+    def enum_creation(name, values)
+      lambda do
+        execute <<~SQL
+          CREATE TYPE #{name}
+            AS ENUM (#{values.map { |s| "'#{s}'" }.join(', ')})
+        SQL
+      end
+    end
+
+    def constraint_creation(table, name, check)
+      lambda do
+        execute <<~SQL
+          ALTER TABLE #{table} ADD CONSTRAINT #{name} CHECK (#{check})
+        SQL
+      end
+    end
+
+    def func_deletion(name)
+      lambda do
+        execute "DROP FUNCTION #{name}"
+      end
+    end
+
+    def enum_deletion(name)
+      lambda do
+        execute "DROP TYPE #{name}"
+      end
+    end
+
+    def constraint_deletion(table, name)
+      lambda do
+        execute "ALTER TABLE #{table} DROP CONSTRAINT #{name}"
       end
     end
   end
