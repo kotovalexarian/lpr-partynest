345b26415741a1f718bb70853ce686d1e251c0f1
Alex Kotov <kotovalexarian@gmail.com>
2019-09-30 16:52:46 +0500

    Improve validations

--- app/validators/good_small_text_validator.rb
+++ app/validators/good_small_text_validator.rb
@@ -7,8 +7,8 @@ class GoodSmallTextValidator < GoodTextValidator
 
     def perform
       super
-      error! :too_short, count: MIN if value.to_s.length < MIN
-      error! :too_long,  count: MAX if value.to_s.length > MAX
+      error :too_short, count: MIN if value.to_s.length < MIN
+      error :too_long,  count: MAX if value.to_s.length > MAX
     end
   end
 end
