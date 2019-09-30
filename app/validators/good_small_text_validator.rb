2f114c165167b7ec3cc9df7d35260f46e3d3c5e7
Alex Kotov <kotovalexarian@gmail.com>
2019-09-30 16:44:24 +0500

    Add GoodSmallTextValidator

---
+++ app/validators/good_small_text_validator.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class GoodSmallTextValidator < GoodTextValidator
+  class Validation < Validation
+    MIN = 1
+    MAX = 255
+
+    def perform
+      super
+      error! :too_short, count: MIN if value.to_s.length < MIN
+      error! :too_long,  count: MAX if value.to_s.length > MAX
+    end
+  end
+end
