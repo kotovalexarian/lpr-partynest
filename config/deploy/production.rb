e26d19081c4fb1fc1d02f477854e35fa8be0cb51
Alex Kotov <kotovalexarian@gmail.com>
2019-10-01 00:02:13 +0500

    Fix code style

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
-server 'alpha.libertarian-party.com', roles: [], no_release: true
+server 'alpha.libertarian-party.com',
+       roles: [],
+       no_release: true
 
-server 'beta.libertarian-party.com', roles: %w[rvm web]
+server 'beta.libertarian-party.com',
+       roles: %w[rvm web]
 
-server 'gamma.libertarian-party.com', roles: %w[rvm web app db]
+server 'gamma.libertarian-party.com',
+       roles: %w[rvm web app db]
