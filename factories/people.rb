c529ce8f7ee8d4d6a021cbdc6aeb65e19b518252
Alex Kotov <kotovalexarian@gmail.com>
2019-10-02 20:17:36 +0500

    Remove unnecessary code

--- factories/people.rb
+++ factories/people.rb
@@ -18,65 +18,49 @@ FactoryBot.define do
 
   factory :supporter_person, parent: :initial_person do
     after :create do |person, _evaluator|
-      create :supporter_relationship,
-             person: person # ,
-      # regional_office: evaluator.regional_office
+      create :supporter_relationship, person: person
     end
   end
 
   factory :member_person, parent: :initial_person do
     after :create do |person, _evaluator|
-      create :member_relationship,
-             person: person # ,
-      # regional_office: evaluator.regional_office
+      create :member_relationship, person: person
     end
   end
 
   factory :excluded_person, parent: :initial_person do
     after :create do |person, _evaluator|
-      create :excluded_relationship,
-             person: person # ,
-      # regional_office: evaluator.regional_office
+      create :excluded_relationship, person: person
     end
   end
 
   factory :federal_manager_person, parent: :initial_person do
     after :create do |person, _evaluator|
-      create :federal_manager_relationship,
-             person: person # ,
-      # regional_office: evaluator.regional_office
+      create :federal_manager_relationship, person: person
     end
   end
 
   factory :federal_supervisor_person, parent: :initial_person do
     after :create do |person, _evaluator|
-      create :federal_supervisor_relationship,
-             person: person # ,
-      # regional_office: evaluator.regional_office
+      create :federal_supervisor_relationship, person: person
     end
   end
 
   factory :regional_manager_person, parent: :initial_person do
     after :create do |person, _evaluator|
-      create :regional_manager_relationship,
-             person: person # ,
-      # regional_office: evaluator.regional_office
+      create :regional_manager_relationship, person: person
     end
   end
 
   factory :regional_supervisor_person, parent: :initial_person do
     after :create do |person, _evaluator|
-      create :regional_supervisor_relationship,
-             person: person # ,
-      # regional_office: evaluator.regional_office
+      create :regional_supervisor_relationship, person: person
     end
   end
 
   factory :regional_secretary_person, parent: :initial_person do
     after :create do |person, _evaluator|
-      create :regional_secretary_relationship,
-             person: person # ,
-      # regional_office: evaluator.regional_office
+      create :regional_secretary_relationship, person: person
     end
   end
 end
