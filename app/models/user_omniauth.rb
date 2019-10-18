fda85b6e4357d1e86e5fa98f63a4351c3d365813
Alex Kotov <kotovalexarian@gmail.com>
2019-10-18 07:56:48 +0500

    Add and validate association UserOmniauth#assumed_user

--- app/models/user_omniauth.rb
+++ app/models/user_omniauth.rb
@@ -7,6 +7,14 @@ class UserOmniauth < ApplicationRecord
 
   belongs_to :user
 
+  has_one :assumed_user,
+          class_name: 'User',
+          primary_key: :email,
+          foreign_key: :email,
+          inverse_of: false,
+          autosave: false,
+          dependent: false
+
   ###############
   # Validations #
   ###############
@@ -16,4 +24,12 @@ class UserOmniauth < ApplicationRecord
   validates :remote_id, presence: true
 
   validates :email, presence: true
+
+  validates :assumed_user, inclusion: { in: :expected_users }
+
+private
+
+  def expected_users
+    [nil, user]
+  end
 end
