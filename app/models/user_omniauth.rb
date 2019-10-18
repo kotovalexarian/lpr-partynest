28d40dd6320c3f714f379aeaab35d3b9a9128d03
Alex Kotov <kotovalexarian@gmail.com>
2019-10-18 07:43:52 +0500

    Add comments and empty lines

--- app/models/user_omniauth.rb
+++ app/models/user_omniauth.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
 class UserOmniauth < ApplicationRecord
+  ################
+  # Associations #
+  ################
+
   belongs_to :user
 
+  ###############
+  # Validations #
+  ###############
+
   validates :provider, presence: true, uniqueness: { scope: :remote_id }
+
   validates :remote_id, presence: true
+
   validates :email, presence: true
 end
