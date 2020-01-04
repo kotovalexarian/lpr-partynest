6846af8668748df8c223ed7136fc1a904ebde7fa
Alex Kotov <kotovalexarian@gmail.com>
2020-01-05 00:09:23 +0500

    Paginate FederalSubjectsController#index

--- app/controllers/federal_subjects_controller.rb
+++ app/controllers/federal_subjects_controller.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 class FederalSubjectsController < ApplicationController
+  include PaginalController
+
   before_action :set_federal_subject, except: :index
 
   # GET /federal_subjects
   def index
     authorize :federal_subject
     @federal_subjects = policy_scope(FederalSubject).order_by_display_name
+                                                    .page(active_page)
   end
 
   # GET /federal_subjects/:number
--- spec/requests/federal_subjects/index_spec.rb
+++ spec/requests/federal_subjects/index_spec.rb
@@ -3,13 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe 'GET /federal_subjects' do
+  let(:current_account) { create :superuser_account }
+
+  let :federal_subjects_count do
+    [0, 1, rand(2..4), rand(5..10), rand(20..40)].sample
+  end
+
   before do
     sign_in current_account.user if current_account&.user
 
-    create_list :federal_subject, rand(1..3)
+    create_list :federal_subject, federal_subjects_count
 
     get '/federal_subjects'
   end
+
+  it_behaves_like 'paginal controller', :federal_subjects_count
 
   for_account_types nil, :usual, :superuser do
     specify do
