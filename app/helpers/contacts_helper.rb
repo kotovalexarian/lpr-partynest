ec1ac0aa7632f326ad2b906b5363643f983ff8ea
Alex Kotov <kotovalexarian@gmail.com>
2020-04-10 10:13:07 +0500

    Add confirmation

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 module ContactsHelper
+  def contact_public_switch_confirmation(contact)
+    return if contact.show_in_public?
+
+    translate :contact_public_switch_confirmation, value: contact.value
+  end
+
   def contact_destroy_confirmation(contact)
     value = "#{Contact.model_name.human(count: 1)} \"#{contact.value}\""
 
