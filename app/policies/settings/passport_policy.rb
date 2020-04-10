6f77301c8f028da17a9dc73f7805614e45034e11
Alex Kotov <kotovalexarian@gmail.com>
2020-04-10 18:53:58 +0500

    Fix code style

--- app/policies/settings/passport_policy.rb
+++ app/policies/settings/passport_policy.rb
@@ -15,32 +15,11 @@ class Settings::PassportPolicy < ApplicationPolicy
 
   def permitted_attributes_for_create
     %i[
-      last_name
-      first_name
-      middle_name
-      sex
-      date_of_birth
-      place_of_birth
-      series
-      number
-      issued_by
-      unit_code
-      date_of_issue
-      zip_code
-      town_type
-      town_name
-      settlement_type
-      settlement_name
-      district_type
-      district_name
-      street_type
-      street_name
-      residence_type
-      residence_name
-      building_type
-      building_name
-      apartment_type
-      apartment_name
+      last_name first_name middle_name sex date_of_birth place_of_birth
+      series number issued_by unit_code date_of_issue zip_code
+      town_type town_name settlement_type settlement_name district_type
+      district_name street_type street_name residence_type residence_name
+      building_type building_name apartment_type apartment_name
     ].freeze
   end
 end
